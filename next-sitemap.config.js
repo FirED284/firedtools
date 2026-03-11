7bebe96ec54be0e7a1ee9c051378cbbeea22ffab
Vlad <firedvk1029384756@gmail.com>
2026-03-11 02:07:32 +0200

    Disable sitemap index, generate single sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://firedtools.vercel.app',
+  generateIndexSitemap: false,
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
