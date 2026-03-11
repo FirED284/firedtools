18b8b96b31640c4a06a000f3978d88e1707db791
Vlad <firedvk1029384756@gmail.com>
2026-03-11 02:02:05 +0200

    Update sitemap base URL to firedtools.vercel.app

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://firedtools.com',
+  siteUrl: process.env.SITE_URL || 'https://firedtools.vercel.app',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
